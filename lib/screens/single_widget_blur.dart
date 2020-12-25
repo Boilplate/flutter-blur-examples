87da47b41881b3297f4e59e972ecec456ff93b1e
FilipLjubic <filip.ljubic@fer.hr>
2020-12-25 18:26:47 +0100

    changed single widget blur

--- lib/screens/single_widget_blur.dart
+++ lib/screens/single_widget_blur.dart
@@ -1,55 +1,24 @@
 import 'dart:ui';
 
+import 'package:blur/widgets/back_arrow.dart';
 import 'package:flutter/material.dart';
 
-class SingleScreen extends StatefulWidget {
-  @override
-  _SingleScreenState createState() => _SingleScreenState();
-}
-
-class _SingleScreenState extends State<SingleScreen>
-    with SingleTickerProviderStateMixin {
-  AnimationController animationController;
-  Animation animation;
-
-  @override
-  void initState() {
-    super.initState();
-    animationController =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 1500))
-          ..forward()
-          ..repeat(reverse: true);
-    animation = Tween<double>(begin: 50, end: 400).animate(
-      CurvedAnimation(
-        curve: Curves.easeIn,
-        parent: animationController,
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    animationController.dispose();
-  }
-
+class SingleScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: animation,
-      builder: (context, child) => Stack(
-        alignment: Alignment.center,
+    return Scaffold(
+      body: Stack(
         children: [
-          Positioned(
-            bottom: 50 + animation.value,
+          Center(
             child: ImageFiltered(
-              imageFilter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+              imageFilter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
               child: Image(
                 image: AssetImage('assets/abstract_art.jpeg'),
                 height: 150,
               ),
             ),
           ),
+          BackArrow(),
         ],
       ),
     );
